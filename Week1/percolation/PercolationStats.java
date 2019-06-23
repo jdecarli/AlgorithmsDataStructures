7cd4c3a1d6beac8e554d79ac3d71dc82c9fa946d
Javier De Carli <jdecarli@users.noreply.github.com>
2019-06-23 15:06:31 -0400

    Adding basic main method implementation with debug mode and fix for getGridCoordinates method

--- Week1/percolation/PercolationStats.java
+++ Week1/percolation/PercolationStats.java
@@ -5,11 +5,14 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
 
 public class PercolationStats {
+    // enable/disable debug mode
+    private static boolean isDebugMode;
+    private static int nTrials = 0;
 
     private int size;
-    private int nTrials;
     private int numberOfCells;
     private int ixVirtualTop;
     private int ixVirtualBottom;
@@ -30,7 +33,6 @@ public class PercolationStats {
         // Set the indices for the virtual top/bottom sites (cells):
         ixVirtualTop = 0;
         ixVirtualBottom = numberOfCells - 1;
-
     }
 
     public double mean()                          // sample mean of percolation threshold
@@ -59,8 +61,20 @@ public class PercolationStats {
         /*
          * Private Methods
          */
-        System.out.println("Started...");
-        // System.out.println("Args: " + args[0] + "," + args[1]);
+        // Debug mode
+        isDebugMode = false;
+
+        System.out.println("Started... (DebugMode: " + isDebugMode + ")\n");
+        Stopwatch stopwatch = new Stopwatch();
+
+        System.out.println("Size: " + args[0] + " | Trials: " + args[1]);
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]),
+                                                      Integer.parseInt(args[1]));
+
+        for (int i = 0; i < nTrials; i++)
+            stats.runNewTrial();
+
+        System.out.println("\nElapsed time: " + stopwatch.elapsedTime());
     }
 
     private int[] getGridCoordinates(int ix) {
@@ -85,7 +99,8 @@ public class PercolationStats {
         int col;
 
         row = ix / size + 1;
-        col = ix - (row - 1) * size;
+        // col = ix - (row - 1) * size;
+        col = ix - (row - 1) * size + 1; // index is 1 based
 
         int[] coord = { row, col };
 
@@ -95,6 +110,9 @@ public class PercolationStats {
     private int[] getShuffledIndexList(int n) {
         // Get the array length
         int numberOfGridCells = n * n;
+
+        if (isDebugMode)
+            System.out.println("shuffled list - numberOfGridCells: " + numberOfGridCells);
 
         // Build a list of cell indices
         int[] ixList = new int[numberOfGridCells];
@@ -119,8 +137,16 @@ public class PercolationStats {
         // Instantiate a new grid for this trial
         Percolation grid = new Percolation(size);
 
+        if (isDebugMode) System.out.println("percolation class ok");
+
         // Get a shuffled index list
         int[] ixListShuffled = getShuffledIndexList(size);
+
+        if (isDebugMode) {
+            System.out.println("shuffled list ok");
+            System.out.println("ixListShuffled - size: " + size);
+            System.out.println("ixListShuffled - ixListShuffled.length: " + ixListShuffled.length);
+        }
 
         // Loop through all cells randomly.
         // Keep opening them until the grid percolates
@@ -130,7 +156,14 @@ public class PercolationStats {
             int[] gridCoord = getGridCoordinates(ixListShuffled[i]);
             int row = gridCoord[0];
             int col = gridCoord[1];
+
             // ... and open it
+            if (isDebugMode) {
+                System.out.println("open - ixListShuffled[i]: " + ixListShuffled[i]);
+                System.out.println("open - index: " + i);
+                System.out.println("open - row: " + row + " | col: " + col);
+            }
+
             grid.open(row, col);
 
             i++;
@@ -144,7 +177,7 @@ public class PercolationStats {
     private void validateInput(int n, int trials) {
         if (n <= 0 || trials <= 0) {
             throw new java.lang.IllegalArgumentException(
-                    "The grid size must be a positive integer!");
+                    "The grid size and the amount of trials must be a positive integer!");
         }
     }
 }
