3d1bad1ec67f9b31679c243dc05c981dae7d097c
Javier De Carli <jdecarli@users.noreply.github.com>
2019-06-25 20:22:54 -0400

    initial commit for week 2 - project + basic apis

---
+++ Week2/queues/Permutation.java
@@ -0,0 +1,10 @@
+/* *****************************************************************************
+ *  Name: KB & JDC
+ *  Date:
+ *  Description: Programming Assignment 2: Permutation
+ **************************************************************************** */
+
+public class Permutation {
+    public static void main(String[] args) {
+    }
+}
---
+++ Week2/queues/RandomizedQueue.java
@@ -0,0 +1,41 @@
+/* *****************************************************************************
+ *  Name: KB & JDC
+ *  Date:
+ *  Description: Programming Assignment 2: RandonmizedQueue
+ **************************************************************************** */
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    public RandomizedQueue()                 // construct an empty randomized queue
+    {
+    }
+
+    public boolean isEmpty()                 // is the randomized queue empty?
+    {
+    }
+
+    public int size()                        // return the number of items on the randomized queue
+    {
+    }
+
+    public void enqueue(Item item)           // add the item
+    {
+    }
+
+    public Item dequeue()                    // remove and return a random item
+    {
+    }
+
+    public Item sample()                     // return a random item (but do not remove it)
+    {
+    }
+
+    public Iterator<Item> iterator()         // return an independent iterator over items in random order
+    {
+    }
+
+    public static void main(String[] args)   // unit testing (optional)
+    {
+    }
+}
