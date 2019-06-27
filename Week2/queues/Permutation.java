0400195db4b517f3b1862c1fe5c8b40eb3eae3bf
superkostya <kostyantyn.borysenko@gmail.com>
2019-06-27 16:16:17 -0400

    Complete the main() method to use the RandomizedQueue class

--- Week2/queues/Permutation.java
+++ Week2/queues/Permutation.java
@@ -16,15 +16,30 @@ public class Permutation {
 
         String s;
         boolean isEmpty = false;
-        StdOut.println("Reading strings from the Standard Input....");
+
+        // Instantiate a Randomized Queue
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+
+        // StdOut.println("Reading strings from the Standard Input....");
         while (!isEmpty) {
             try {
                 s = StdIn.readString();
-                StdOut.println(s);
+                // StdOut.println(s);
+                rq.enqueue(s);
             }
             catch (NoSuchElementException e) {
                 isEmpty = true;
             }
+        }
+
+        // Print `numPops` random elements from the queue
+        // StdOut.println(
+        //         "Printing " + numPops + " randomly chosen strings from the Standard Input....");
+        int counter = 0;
+        for (String s1 : rq) {
+            StdOut.println(s1);
+            counter++;
+            if (counter == numPops) break;
         }
 
 
