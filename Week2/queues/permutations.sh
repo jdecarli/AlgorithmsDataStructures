181d67f4933b96636834fadb1afccdc7d32fe353
Javier De Carli <jdecarli@users.noreply.github.com>
2019-06-28 21:32:17 -0400

    adding bash script with classpath

---
+++ Week2/queues/permutations.sh
@@ -0,0 +1,4 @@
+classpath=/d/Code/GitHub/MyRepos/AlgorithmsDataStructures/Week2/queues:/d/Code/GitHub/MyRepos/AlgorithmsDataStructures/Week2/queues/.lift/algs4.jar
+
+# java -classpath $classpath Permutation 3 < distinct.txt
+java -classpath $classpath Permutation 0 < duplicates.txt
