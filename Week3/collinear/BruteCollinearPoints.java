da783630a360b9e484d9e4be7ff8a2030a3e8aa9
superkostya <kostyantyn.borysenko@gmail.com>
2019-07-05 23:03:24 -0400

    Finish the 1st version of the Brute

--- Week3/collinear/BruteCollinearPoints.java
+++ Week3/collinear/BruteCollinearPoints.java
@@ -4,17 +4,22 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Comparator;
 
 public class BruteCollinearPoints {
 
+    private Point[] points;
     private LineSegment[] segmentsOfFour;
     private int numOfSegments;
 
     public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
     {
-        segmentsOfFour = new LineSegment[points.length / 4];
-        numOfSegments = 0;
+        this.points = points;
+        this.segmentsOfFour = new LineSegment[points.length * points.length];
+        this.numOfSegments = 0;
+
         for (int i = 0; i < points.length; i++) {
             for (int j = i + 1; j > i && j < points.length; j++) {
                 for (int k = j + 1; k > j && k < points.length; k++) {
@@ -29,7 +34,7 @@ public class BruteCollinearPoints {
                         if (pointSlopeComparator.compare(pj, pk) == 0) {
                             if (pointSlopeComparator.compare(pj, pq) == 0) {
                                 LineSegment segmentOf4 = new LineSegment(pi, pq);
-                                segmentsOfFour[numOfSegments] = segmentOf4;
+                                segmentsOfFour[numOfSegments++] = segmentOf4;
                             }
                         }
                     }
@@ -49,6 +54,28 @@ public class BruteCollinearPoints {
     }
 
     public static void main(String[] args) {
+
+        Point[] points = new Point[10];
+
+        for (int i = 0; i < 6; i++) {
+            points[i] = new Point(i, i);
+        }
+
+        points[6] = new Point(0, 1);
+        points[7] = new Point(0, 2);
+        points[8] = new Point(0, 3);
+        points[9] = new Point(0, 4);
+
+        BruteCollinearPoints bcp = new BruteCollinearPoints(points);
+
+        StdOut.println("Number of points: " + points.length);
+
+        StdOut.println("Number of discovered 4-point segments: " + bcp.numberOfSegments());
+
+        for (LineSegment segment : bcp.segments()) {
+            StdOut.println(segment.toString());
+        }
+
 
     }
 }
