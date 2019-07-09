a1b19f9b2d0b632fe5f2412041e82c70296fe9da
superkostya <kostyantyn.borysenko@gmail.com>
2019-07-09 13:49:39 -0400

    Use the provided MergeSort (algs4) and finish the Constructor

--- Week3/collinear/FastCollinearPoints.java
+++ Week3/collinear/FastCollinearPoints.java
@@ -1,76 +1,64 @@
+import edu.princeton.cs.algs4.MergeX;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class FastCollinearPoints {
 
+    private LineSegment[] segmentArr;
+    private int numSegments;
+
     public FastCollinearPoints(
             Point[] points)     // finds all line segments containing 4 or more points
     {
         int n = points.length;
+
+        numSegments = 0;
+        segmentArr = new LineSegment[n * n];
+
         // Sort the points
         Arrays.sort(points, Point::compareTo);
         // Go over all points
         for (int i = 0; i < n; i++) {
-            // Pick the focal point and create a slope comparator
+            // Pick the "Focal Point" and create a slope comparator
             Comparator<Point> pointSlopeComparator = points[i].slopeOrder();
             // Subarray of points: all those to the right from Point i
             Point[] pointsToCompare = Arrays.copyOfRange(points, i, n - 1);
-            // Do the Merge Sort of the slopes
-            mergeSort(pointsToCompare, pointSlopeComparator);
+            // Do the Merge Sort of the Points, according to their slopes
+            // (with respect to the Focal Point)
+            MergeX.sort(pointsToCompare, pointSlopeComparator);
+            // mergeSort(pointsToCompare, pointSlopeComparator);
+            for (int j = 0; j < pointsToCompare.length; j++) {
+                Point firstPoint = pointsToCompare[j];
+                int pointCounter = 2;
+
+                for (int k = 1; j + k < pointsToCompare.length; k++) {
+                    if (pointSlopeComparator.compare(firstPoint, pointsToCompare[j + k]) == 0) {
+                        pointCounter++;
+                    }
+                    else {
+                        if (pointCounter > 3) {
+                            this.segmentArr[this.numSegments++] = new LineSegment(firstPoint,
+                                                                                  pointsToCompare[
+                                                                                          j + k
+                                                                                                  - 1]);
+                        }
+                        break;
+                    }
+                }
+
+            }
         }
     }
 
     public int numberOfSegments()        // the number of line segments
     {
-        return 0;
+        return this.numSegments;
     }
 
     public LineSegment[] segments()                // the line segments
     {
-        return null;
+        return this.segmentArr;
     }
-
-    private static void mergeSort(Object[] arr, Comparator comp) {
-        // Create the auxiliary array
-        Object[] aux = new Object[arr.length];
-        // Run the Sort
-        sort(arr, aux, 0, arr.length - 1, comp);
-    }
-
-    private static void sort(Object[] arr, Object[] aux, int lo, int hi,
-                             Comparator comp) {
-        if (hi <= lo) return;
-        // Define the mid-point index in the array
-        int mid = lo + (hi - lo) / 2;
-        // Sort the left half
-        sort(arr, aux, lo, mid, comp);
-        // Sort the right half
-        sort(arr, aux, mid + 1, hi, comp);
-        // Merge them
-        merge(arr, aux, lo, mid, hi, comp);
-    }
-
-    private static void merge(Object[] arr, Object[] aux, int lo, int mid, int hi,
-                              Comparator comp) {
-
-        // TODO: Implement isSorted().
-        // assert isSorted(a, lo, mid); // precondition: a[lo..mid] sorted
-        // assert isSorted(a, mid+1, hi); // precondition: a[mid+1..hi] sorted
-
-        // Fill the Aux array
-        for (int k = lo; k <= hi; k++)
-            aux[k] = arr[k];
-        // Initialize the indices i and j
-        int i = lo, j = mid + 1;
-
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid) arr[k] = aux[j++];
-            else if (j > hi) arr[k] = aux[i++];
-            else if (comp.compare(aux[j], aux[i]) < 0) arr[k] = aux[j++];
-            else arr[k] = aux[i++];
-        }
-        // assert isSorted(a, lo, hi); // postcondition: a[lo..hi] sorted
-    }
-
 
 }
