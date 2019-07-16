81a33c4828ee8820ef6265ad67c59d479a0bb0d9
Javier De Carli <jdecarli@users.noreply.github.com>
2019-07-15 20:17:16 -0400

    adding api for week 4 assignment

---
+++ Week4/8puzzle/Solver.java
@@ -0,0 +1,29 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Solver {
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+    }
+
+    // min number of moves to solve initial board
+    public int moves() {
+    }
+
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+    }
+
+}
