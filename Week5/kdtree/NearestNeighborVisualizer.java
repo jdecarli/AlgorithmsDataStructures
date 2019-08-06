ac6d97edd7fc6fec0d497db0514afabf334082c0
superkostya <kostyantyn.borysenko@gmail.com>
2019-08-06 14:39:22 -0400

    Comment out the KD-tree related lines (for debugging)

--- Week5/kdtree/NearestNeighborVisualizer.java
+++ Week5/kdtree/NearestNeighborVisualizer.java
@@ -23,12 +23,12 @@ public class NearestNeighborVisualizer {
         String filename = args[0];
         In in = new In(filename);
         PointSET brute = new PointSET();
-        KdTree kdtree = new KdTree();
+        // KdTree kdtree = new KdTree();
         while (!in.isEmpty()) {
             double x = in.readDouble();
             double y = in.readDouble();
             Point2D p = new Point2D(x, y);
-            kdtree.insert(p);
+            // kdtree.insert(p);
             brute.insert(p);
         }
 
@@ -54,8 +54,8 @@ public class NearestNeighborVisualizer {
             StdDraw.setPenRadius(0.02);
 
             // draw in blue the nearest neighbor (using kd-tree algorithm)
-            StdDraw.setPenColor(StdDraw.BLUE);
-            kdtree.nearest(query).draw();
+            // StdDraw.setPenColor(StdDraw.BLUE);
+            // kdtree.nearest(query).draw();
             StdDraw.show();
             StdDraw.pause(40);
         }
