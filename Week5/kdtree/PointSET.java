f5141e71b35ff89feb1255a3f904c5e7935be079
Javier De Carli <jdecarli@users.noreply.github.com>
2019-09-04 17:00:32 -0400

    adding first possible brute force implementation

--- Week5/kdtree/PointSET.java
+++ Week5/kdtree/PointSET.java
@@ -8,9 +8,9 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 public class PointSET {
-
     private final SET<Point2D> internalPointSet;
 
     public PointSET()                               // construct an empty set of points
@@ -36,6 +36,8 @@ public class PointSET {
 
         if (this.contains(p))
             return;
+
+        this.internalPointSet.add(p);
 
         // Your implementation should support insert() and contains() in time proportional to the
         // logarithm of the number of points in the set in the worst case; it should support nearest()
@@ -63,6 +65,7 @@ public class PointSET {
             point.draw();
         }
 
+        // TODO: draw lines
         // Horizontal (blue)
         StdDraw.setPenColor(StdDraw.BLUE);
         StdDraw.setPenRadius();
@@ -84,6 +87,21 @@ public class PointSET {
 
         if (this.isEmpty())
             return null;
+
+        // TODO: not implemented - check one by one (BF approach)
+        SET<Point2D> pointsInRange = new SET<Point2D>();
+
+        // cycle thru all points
+        for (Point2D point : this.internalPointSet) {
+            // rectangle contains
+
+            Debug("range | " + point.toString());
+
+            if (rect.contains(point))
+                pointsInRange.add(point); // if yes -> add to range set
+        }
+
+        return pointsInRange;
     }
 
     public Point2D nearest(
@@ -94,9 +112,84 @@ public class PointSET {
 
         if (this.isEmpty())
             return null;
+
+        // TODO: not implemented - check one by one (BF approach)
+        Point2D result = new Point2D(0, 0);
+        double minimunDistance = 1;
+
+        // cycle thru all points
+        for (Point2D point : this.internalPointSet) {
+            double distance = p.distanceTo(point);
+            Debug("nearest | distance: " + distance);
+
+            if (distance < minimunDistance) {
+                minimunDistance = distance;
+                result = point;
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
+        StdOut.println("-------------------------------------");
+        StdOut.println("Initialize");
+        StdOut.println("-------------------------------------");
 
+        StdOut.println("Empty -------------------------------");
+        PointSET set = new PointSET();
+        StdOut.println("Empty (true): " + set.isEmpty());
+        Point2D point = new Point2D(0.6, 0.5);
+        set.insert(point);
+        StdOut.println("Insert p1 + Empty (false): " + set.isEmpty());
+
+        StdOut.println("Contains ----------------------------");
+        Point2D point2 = new Point2D(0, 0.9);
+        set.insert(point2);
+        StdOut.println("Contains p1: " + set.contains(point));
+        Point2D point3 = new Point2D(0.1, 0.4);
+        StdOut.println("Contains p3 (false): " + set.contains(point3));
+
+        StdOut.println("Size --------------------------------");
+        StdOut.println("Size (2): " + set.size());
+
+        StdOut.println("Range --------------------------------");
+        RectHV range = new RectHV(0.0, 0.0, 0.9, 0.9);
+        StdOut.println("Range: " + range);
+        for (Point2D p : set.range(range)) {
+            StdOut.println(p);
+        }
+
+        StdOut.println("Nearest ------------------------------");
+        Point2D point4 = new Point2D(0.2, 0.2);
+        set.insert(point4);
+        StdOut.println("Add p4: " + point4);
+        Point2D searchPoint = new Point2D(0.1, 0.1);
+        Point2D result = set.nearest(searchPoint);
+        StdOut.println("Nearest (0.2, 0.2): " + result);
+
+        // Show ALL points
+        GetAll(set);
+    }
+
+    // TODO: DELETE BELOW METHODS ONCE READY ----------------------
+
+    private static boolean IsDebugEnabled = false;
+
+    private static void Debug(String message) {
+        if (IsDebugEnabled)
+            StdOut.println("Debug - " + message);
+    }
+
+    // exposed testing method
+    private static void GetAll(PointSET set) {
+        StdOut.println("\nAll points ------------------------------");
+        for (Point2D p : set.GetAll()) {
+            StdOut.println(p);
+        }
+    }
+
+    public Iterable<Point2D> GetAll() {
+        return this.internalPointSet;
     }
 }
